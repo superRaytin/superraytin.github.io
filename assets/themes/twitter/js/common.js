15262a7483809e3a18bc264d902a6c6d45950f6d
superRaytin <superRaytin@163.com>
2013-05-30 19:25:36 +0800

    -t

---
+++ assets/themes/twitter/js/common.js
@@ -0,0 +1,44 @@
+/**
+ * common.js.
+ * User: raytin
+ * Date: 13-5-30
+ */
+var _ = {
+    domReady: function(start){
+        if(document.addEventListener){
+            document.addEventListener('DOMContentLoaded', start, false);
+        }else{
+            window.onload = start;
+        }
+    },
+    $: function(id){
+        return document.getElementById(id);
+    },
+    bind: function(el, type, fun){
+        if(document.addEventListener){
+            el.addEventListener(type, fun, false);
+        }
+        else if(document.attachEvent){
+            el.attachEvent('on' + type, fun)
+        }
+        else{
+            el['on' + type] = fun;
+        }
+    }
+}
+
+var done = {
+    ev: function(){
+        var disqus = _.$('disqus_thread'),
+            btn_showcom = _.$('J-showComment');
+
+        _.bind(btn_showcom, function(){
+            btn_showcom.parentNode.className = 'hide';
+            disqus.className = '';
+        });
+    },
+    init: function(){
+        this.ev();
+    }
+}
+done.init();
